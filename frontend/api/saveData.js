c6fe3a033c005a922f6201ab748eb7c984e807b9
Nathan Yuan <NYuan@Nathans-Macbook-PM-GCF399J3WH.local>
2025-10-06 12:21:38 -0700

    Add sucess 200 message

--- frontend/api/saveData.js
+++ frontend/api/saveData.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
     result = await supabase
       .from(table)
       .insert(data);
+    return res.status(200).json({ message: 'Bug ticket saved successfully' });
   }
 
   if (table === 'ChatLog') {
